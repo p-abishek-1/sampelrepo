93a1f9c8b736fe2ed9a767832efaa8cba287f3ee
Abishek.P <perumalabishek23@gmail.com>
2024-08-01 13:46:51 +0530

    updated the errors
    vefifed the every errors using comlier

--- first program.c
+++ first program.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
-int main
+int main()
 {
     int a,b,sum,sub,ope;
     printf("enter the a and b:");
-    scanf("%d%d",&a,&b&);
+    scanf("%d%d",&a,&b);
     printf("enter the ope:");
     scanf("%d",&ope);
-    if(ope=1)
+    if(ope==1)
+    {
         sum=a+b;
         printf("sum=%d",sum);
-    else if(ope=2)
+    }
+    else if(ope==2)
+    {
         sub=a-b;
         printf("sub=%d",sub);
+    }
     return 0;
 }
